3a363a7329684cca813ee670caaa790454b132e1
Myselfmk Apps (A Development Company) <myselfmkapps@gmail.com>
2025-09-12 14:16:27 +0000

    Remove  (_for element p_)

--- src/components/sections/apps.tsx
+++ src/components/sections/apps.tsx
@@ -9,9 +9,6 @@ export function Apps() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">My Applications</h2>
-            <p className="max-w-[900px] text-foreground/80 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Here's a selection of apps I've designed and developed. Each project showcases my commitment to quality and user experience.
-            </p>
           </div>
         </div>
         <div className="mt-12 space-y-12">
